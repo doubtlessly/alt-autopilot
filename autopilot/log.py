126646c209286b34f80a1f43c81309adba56dcfa
doubtlessly <yasperfriese@live.com>
2025-08-25 22:16:05 +1200

    Create log.py

---
+++ autopilot/log.py
@@ -0,0 +1,9 @@
+import logging
+def get_logger(name: str = "autopilot") -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        h = logging.StreamHandler()
+        h.setFormatter(logging.Formatter("%(asctime)s | %(levelname)s | %(message)s"))
+        logger.addHandler(h)
+    return logger
